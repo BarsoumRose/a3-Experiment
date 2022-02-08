a214c87f1a0faeed718f299a7aacf2753b319d75
Gabi Tessier <gabi246>
2022-02-08 11:16:16 -0500

    finished color and greyscale pie chart

--- popularChartData.R
+++ popularChartData.R
@@ -5,7 +5,17 @@ entries <- floor(runif(numEntries, min=0, max=40))
 
 chartNum <- floor(runif(1, min=0, max=3))
 
+i=1
+while ((numEntries+1) != i){
+  val = paste('section', i, sep=" ")
+  val
+  newList[i] <- val
+  newList
+  i <- i+1
+}
+
 df <- data.frame( chart = chartNum,
+                  name = p,
                   value = entries)
 
-write.csv(df,"testData.csv")
+write.csv(df,"C:/Users/Gabi/Documents/WPI CS/Data Vision/a3-Experiment/testData.csv", row.names = FALSE)
