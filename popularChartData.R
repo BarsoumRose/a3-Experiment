d30522e1fde8c07f69171e57037cd4e6b9d77891
Gabi Tessier <gabi246>
2022-02-17 20:28:37 -0500

    changing test data

--- popularChartData.R
+++ popularChartData.R
@@ -1,11 +1,26 @@
 
+
 numEntries = 7
+bool <- c(false,true,false,false,false,false,false)
 
 entries <- floor(runif(numEntries, min=0, max=40))
 
+x <- floor(runif(numEntries, min=0, max=10))
+
 chartNum <- floor(runif(1, min=0, max=3))
 
-df <- data.frame( chart = chartNum,
-                  value = entries)
+i=1
+while ((numEntries+1) != i){
+  val = paste('section', i, sep=" ")
+  val
+  newList[i] <- val
+  newList
+  i <- i+1
+}
 
-write.csv(df,"testData.csv")
+df <- data.frame( chart = chartNum,
+                  xval = x,
+                  value = entries,
+                  selected = bool)
+
+write.csv(df,"C:/Users/Gabi/Documents/WPI CS/Data Vision/a3-Experiment/testData.csv", row.names = FALSE)
